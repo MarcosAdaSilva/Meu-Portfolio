f2c80f916a62c506a78a2e962d23009f15db0922
MarcosAdaSilva <marcaofly@gmail.com>
2024-01-31 23:44:13 -0300

    Escrevendo css e javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new typed(".text", {
+  Strings: [
+    "Desenvolvedor Frontend",
+    "Desenvolvedor Backend",
+    "Desenvolvedor Web",
+  ],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
